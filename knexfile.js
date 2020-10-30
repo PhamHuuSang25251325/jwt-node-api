d81ceb43aed399e965ef90d1c750ebc77ad03e0f
meocon200890 <meocon200890@gmail.com>
2020-10-31 00:01:56 +0700

    config Knex

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: '123456',
+      database: 'test-knex'
+    },
+     useNullAsDefault: true
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+ 
+
+};
