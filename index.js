9db8dbb10167eda7541beb2a5ae7fb2cae838c3b
meocon200890 <meocon200890@gmail.com>
2020-10-31 00:03:56 +0700

    change code index

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
 
 
-app.get("/",(req,res)=>{
-    res.send('Hello worldxxx!!')
+require('dotenv').config()
+const knex = require('./dbConfig');
+const authRoutes = require('./routes/authRouter');
+const taskRoutes = require('./routes/taskRouter');
+
+
+
+
+app.get("/", async(req,res)=>{
+    const users =await knex.select().table('users')
+    res.send(`length ${users.length}`)
 })
 
-const PORT = process.env.PORT || 5000;
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
+app.use('/',authRoutes)
+app.use('/',taskRoutes)
+
+
+const PORT = process.env.PORT || 5000;
+ 
 app.listen(PORT,()=>{
     console.log(`sever run is port ${PORT}`)
 })
